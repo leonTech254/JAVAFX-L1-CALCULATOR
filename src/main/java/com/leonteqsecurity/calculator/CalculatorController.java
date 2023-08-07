b710d4b6f4ec695b27c562b92769b5acecd69fbc
leonTech254 <martinleontech23@gmail.com>
2023-08-07 21:50:31 +0300

    --changed-logic to list

--- src/main/java/com/leonteqsecurity/calculator/CalculatorController.java
+++ src/main/java/com/leonteqsecurity/calculator/CalculatorController.java
@@ -4,10 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculatorController {
     private String currentNumberString="";
     private int parentNumber=0;
     private  String currentOperation;
+    private  int finalResults;
+
+    private List<String> AllData=new ArrayList<>();
 
     @FXML
     public  void  buttonClicked(ActionEvent event)
@@ -37,36 +43,48 @@ public class CalculatorController {
         String operator=buttonclicked.getText();
         currentOperation= operator;
 //        System.out.println(currentOperation);
-        parentNumber =Integer.parseInt(currentNumberString);
+        AllData.add(currentNumberString);
+        AllData.add(currentOperation);
+        currentOperation="";
+        currentNumberString="";
     }
     public  void equal()
     {
 //        System.out.println(parentNumber+" "+currentNumberString);
-        myFactory(parentNumber, Integer.parseInt(currentNumberString),currentOperation);
+        AllData.add(currentNumberString);
+        currentNumberString="";
+        myFactory(AllData);
     }
-    public int myFactory(int existingNumber, int currentNumber, String symbol)
+    public int myFactory(List<String> allData) {
+        int result = Integer.parseInt(allData.get(0)); // Initialize result with the first number
+System.out.println(allData);
+        for (int i = 1; i < allData.size(); i += 2) {
+            String operator = allData.get(i);
+            // Make sure we have a valid index for the next number
+            if (i + 1 < allData.size()) {
+                if (!operator.isEmpty())
+                {
 
-    {
-        int results = 0;
-        if (symbol.equals("+"))
-        {
-             results=existingNumber+currentNumber;
+                int currentNumber = Integer.parseInt(allData.get(i + 1));
 
-        }else if (symbol.equals("-"))
-        {
-             results=existingNumber-currentNumber;
+                if (operator.equals("+")) {
+                    result += currentNumber;
+                } else if (operator.equals("-")) {
+                    result -= currentNumber;
+                } else if (operator.equals("*")) {
+                    result *= currentNumber;
+                } else if (operator.equals("/")) {
+                    result /= currentNumber;
+                }
+            }
+            }
+
         }
-        else if (symbol.equals("*"))
-        {
-             results=existingNumber*currentNumber;
-        }else if (symbol.equals("/"))
-        {
-             results=existingNumber/currentNumber;
-        }
-        System.out.println(results);
-        return results;
 
-
+        System.out.println(result);
+        return result;
     }
+
+
 
 }
